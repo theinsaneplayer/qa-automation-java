7ff10f105410f8ef2250961c112118f9a0bcffb2
p.shatskov <p.shatskov@tinkoff.ru>
2022-03-27 13:14:32 +0300

    Added javaDoc for ConsolePrinter.java

--- src/main/java/ConsolePrinter.java
+++ src/main/java/ConsolePrinter.java
@@ -1,3 +1,9 @@
+/**
+ * Класс используется для вывода сообщений в консоль.
+ * Метод print печатает сообщения для вывода в консоль.
+ * @param  message принимает строки
+ * @author p.shatskov
+ */
 public class ConsolePrinter {
     public static void print(String message) {
         System.out.println(message);
