d670e1f3d7d569b81698867bc90ce142cf74a811
p.shatskov <p.shatskov@tinkoff.ru>
2022-04-10 22:16:31 +0300

    feat: added conditional operator for decorate

--- src/main/java/com/tcs/edu/Application.java
+++ src/main/java/com/tcs/edu/Application.java
@@ -5,5 +5,10 @@ import static com.tcs.edu.decorator.TimestampMessageDecorator.decorate;
 public class Application {
     public static void main(String[] args) {
         decorate("Hello world!");
+        decorate("Hello world!");
+        decorate("Hello world!");
+        decorate("Hello world!");
+        decorate("Hello world!");
+        decorate("Hello world!");
     }
 }
--- src/main/java/com/tcs/edu/decorator/TimestampMessageDecorator.java
+++ src/main/java/com/tcs/edu/decorator/TimestampMessageDecorator.java
@@ -8,6 +8,7 @@ import java.time.Instant;
  */
 public class TimestampMessageDecorator {
     public static int messageCount = 1;
+    public static final int PAGE_SIZE = 2;
 
     /**
      * Метод берёт текущаю дату и время, соединяя их с заданным сообщением.
@@ -16,7 +17,12 @@ public class TimestampMessageDecorator {
      * @author p.shatskov
      */
     public static void decorate(String message) {
-        final var decoratedMessage = Instant.now() + " " + message;
-        System.out.println((messageCount++) + " " + decoratedMessage);
+
+        final var decorate = Instant.now() + " " + message;
+        final var counter = messageCount++;
+        if(messageCount % PAGE_SIZE == 0) {
+            System.out.println("---");
+        }
+        System.out.println(counter + " " + decorate);
     }
 }
