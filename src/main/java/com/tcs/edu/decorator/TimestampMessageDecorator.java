993a9dc3b6466e61ccf85ce8ac43c3ec64b629c0
p.shatskov <p.shatskov@tinkoff.ru>
2022-04-14 10:43:00 +0300

    fix: corrected review remarks

--- src/main/java/com/tcs/edu/decorator/TimestampMessageDecorator.java
+++ src/main/java/com/tcs/edu/decorator/TimestampMessageDecorator.java
@@ -1,28 +1,29 @@
 package com.tcs.edu.decorator;
 
-import java.time.Instant;
+import static java.time.Instant.*;
 
 /**
  * Класс выводит сообщения с временной отметкой
  * @author p.shatskov
  */
 public class TimestampMessageDecorator {
-    public static int messageCount = 1;
+    public static int messageCount = 0;
     public static final int PAGE_SIZE = 2;
 
     /**
      * Метод берёт текущаю дату и время, соединяя их с заданным сообщением.
      * Добавлен счётчик на каждый вывод.
-     * @param message
+     * @param message принимает строку
      * @author p.shatskov
      */
     public static void decorate(String message) {
 
-        final var decorate = Instant.now() + " " + message;
-        final var counter = messageCount++;
-        if(messageCount % PAGE_SIZE == 0) {
+        final var output = String.format("%s %s", now(), message);
+
+        if(messageCount++ % PAGE_SIZE == 0) {
             System.out.println("---");
         }
-        System.out.println(counter + " " + decorate);
+        System.out.println(output);
+
     }
 }
