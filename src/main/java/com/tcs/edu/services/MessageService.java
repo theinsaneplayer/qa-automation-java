5c81e6e3c4b696085b5425a95b771697254da16f
p.shatskov <p.shatskov@tinkoff.ru>
2022-05-16 10:36:44 +0300

    feat: resolved #07-1

--- src/main/java/com/tcs/edu/services/MessageService.java
+++ src/main/java/com/tcs/edu/services/MessageService.java
@@ -2,6 +2,8 @@ package com.tcs.edu.services;
 
 import com.tcs.edu.decorator.Severity;
 
+import java.util.Objects;
+
 import static com.tcs.edu.decorator.SeverityDecorator.getSeverityValueByType;
 import static com.tcs.edu.decorator.TimestampMessageDecorator.*;
 import static com.tcs.edu.printer.ConsolePrinter.print;
@@ -9,17 +11,21 @@ import static com.tcs.edu.printer.ConsolePrinter.print;
 
 /**
  * Класс обрабатывает сообщения согласно бизнес-логике
+ *
  * @author p.shatskov
  */
 public class MessageService {
     /**
      * Метод склеивает декорировании сообщение и уровень значимости
-     * @author p.shatskov
+     *
      * @param Severity,messages Уровень значимости и массив строк сообщений
+     * @author p.shatskov
      */
-    public static void process(Severity level, String... messages) {
-        for (String current : messages) {
-            print(decorate(current) + " " + getSeverityValueByType(level));
+    public static void process(Severity level, String message, String... messages) {
+        if (Objects.equals(message, messages)) {
+            for (String current : messages) {
+                print(decorate(current) + " " + getSeverityValueByType(level));
+            }
         }
     }
 }
